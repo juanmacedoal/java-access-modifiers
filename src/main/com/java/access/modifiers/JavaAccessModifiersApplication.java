410a5b219458c064466277a1027104599e975718
= <juan-manuel.macedo@soprasteria.com>
2022-02-18 14:34:14 +0100

    begin project

---
+++ src/main/com/java/access/modifiers/JavaAccessModifiersApplication.java
@@ -0,0 +1,12 @@
+package main.com.java.access.modifiers;
+
+/** Java Access Modifiers application. */
+public class JavaAccessModifiersApplication {
+
+  /**
+   * Main Java Application.
+   *
+   * @param args Java Args program
+   */
+  public static void main(String[] args) {}
+}
